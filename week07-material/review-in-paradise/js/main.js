51c8b42f8e2656002860039514b9aa9282d8dcaf
Cyd Villavicencio <villacv@gmail.com>
2022-04-28 17:18:03 -0400

    added more RC assignments

---
+++ week07-material/review-in-paradise/js/main.js
@@ -0,0 +1,30 @@
+// *Variables*
+// Declare a variable, reassign it to your favorite food, and alert the value
+let favFood = 'eggs'
+favFood = 'avocado'
+alert(favFood)
+
+
+
+//Declare a variable, assign it a string, alert the second character in the string (Use your google-fu and the MDN)
+let str = 'Bob'
+alert(str[1])
+LERT(str.charAt(1))
+
+// *Functions*
+// Create a function that takes in 3 numbers. Divide the first two numbers and multiply the last. Alert the product. Call the function.
+const divideAndMultipl = (n1, n2, n3) => {
+    let product = (n1/n2) * 3
+    alert(product)
+}
+divideAndMultipl(13, 2, 4)
+
+// Create a function that takes in 1 number. Console log the cube root of the number. Call the function.
+
+
+// *Conditionals*
+//Create a function that takes in a month. If it is a summer month alert "YAY". If another other month, alert "Booo"
+
+
+//*Loops*
+//Create a function that takes in a number. Console log every number from 1 to that number while skipping multiples of 5.
